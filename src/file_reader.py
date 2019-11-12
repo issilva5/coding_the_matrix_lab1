374fcb586ff4969154487f29a06db11194a55ec8
Ítallo de Sousa Silva <itallo.silva@ccc.ufcg.edu.br>
2019-11-12 08:15:51 -0300

    Update file_reader.py

--- src/file_reader.py
+++ src/file_reader.py
@@ -2,6 +2,7 @@
 
 import csv
 from senador import *
+from vetor import *
 
 '''
 Leitura e manipulação do arquivo
@@ -19,6 +20,6 @@ def ler_votacao(path):
 		sen_estado = row[1]
 		sen_partido = row[2]
 		sen_votos = list(map(int, row[3:len(row)]))
-		voting_dict[sen_nome] = Senador(sen_nome, sen_estado, sen_partido, sen_votos)
+		voting_dict[sen_nome] = Senador(sen_nome, sen_estado, sen_partido, Vetor(sen_votos))
 
 	return voting_dict
